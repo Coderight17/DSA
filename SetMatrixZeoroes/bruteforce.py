06cda08a3a1690891ec2032056ad3951e6253d60
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2024-12-01 11:34:54 +0530

    Passes 200/202 test cases
    Contains only  the function, not the whole code

--- SetMatrixZeoroes/bruteforce.py
+++ SetMatrixZeoroes/bruteforce.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    for k in range(len(matrix)):
+                        if matrix[k][j] != 0:
+                            matrix[k][j] = -1
+                    for k in range(len(matrix[i])):
+                        if matrix[i][k] != 0:
+                            matrix[i][k] = -1
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == -1:
+                    matrix[i][j] = 0
