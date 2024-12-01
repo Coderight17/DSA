4508318890a983ba8f7025f191efdbe583fd8416
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2024-12-01 14:54:34 +0530

    Update pascals_triangle.py

--- Pascals_triangle/pascals_triangle.py
+++ Pascals_triangle/pascals_triangle.py
@@ -1,1 +1,13 @@
-
+#Only the function, need to write the rest of the code like getting input, printing etc to get a completely working one.
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascals_triangle = []
+        for i in range(numRows):
+            row = [0]*(i+1)
+            pascals_triangle.append(row)
+            for j in range(len(row)):
+                if j == 0 or j== i:
+                    pascals_triangle[i][j] = 1
+                else:
+                    pascals_triangle[i][j] = pascals_triangle[i-1][j] + pascals_triangle[i-1][j-1]
+        return pascals_triangle   
