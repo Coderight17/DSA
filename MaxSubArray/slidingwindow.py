535fdcd3dded14be6a702de18612b67c8ea86793
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2024-12-01 11:42:35 +0530

    Code added with only the major important function

--- MaxSubArray/slidingwindow.py
+++ MaxSubArray/slidingwindow.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sum = 0
+        maxsum = nums[0]
+        for item in nums:
+            sum = sum + item
+            if sum > maxsum:
+                maxsum = sum
+            if sum <0:
+                sum = 0
+        return maxsum
