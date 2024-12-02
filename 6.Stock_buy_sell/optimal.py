668f1ae1f4384086f247d89b872981a4e4da1545
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2024-12-02 13:12:14 +0530

    Optimal approach done

--- 6.Stock_buy_sell/optimal.py
+++ 6.Stock_buy_sell/optimal.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def maxProfit(self, prices) -> int:
+        min = prices[0]
+        profit = 0
+        for i in range(1,len(prices)):
+            if prices[i-1]<min:
+                min = prices[i-1]
+            if prices[i]-min > profit:
+                profit = prices[i]-min
+        return profit
+prices = [7,1,5,3,6,4]
+s = Solution()
+max_profit = s.maxProfit(prices)
+print(max_profit)
