8d3df2b9d6f8fee9cffc1085811b0cf784be00c7
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2024-12-02 14:20:06 +0530

    Added code

--- Arrays2/7.Rotate_image/rotate_image.py
+++ Arrays2/7.Rotate_image/rotate_image.py
@@ -1,1 +1,14 @@
+class Solution:
+    def rotate(self, matrix) -> None:
+        for i in range(len(matrix)-1):
+            for j in range(i+1, len(matrix[0])):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in matrix:
+            row.reverse()
 
+s = Solution()
+matrix = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+s.rotate(matrix)
+print(matrix)
