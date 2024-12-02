c29fcf2ec73cb92768fed653cc8d577b9cad13b7
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2024-12-02 12:46:17 +0530

    Brute Force approach done

--- 6.Stock_buy_sell/brute_force.py
+++ 6.Stock_buy_sell/brute_force.py
@@ -1,1 +1,14 @@
+class Solution:
+    def maxProfit(self, prices) -> int:
+        max = 0
+        for i in range(len(prices)-1):
+            bp = prices[i]
+            for j in range(i+1, len(prices)):
+                if prices[j]-prices[i] > max:
+                    max = prices[j] - prices[i]
+        return max
 
+prices = [7,1,5,3,6,4]
+s = Solution()
+max_profit = s.maxProfit(prices)
+print(max_profit)
