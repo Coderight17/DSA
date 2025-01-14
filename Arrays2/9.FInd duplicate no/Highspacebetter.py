4a102f30b7c3375b3afbcbbbcb76bc4bd6686de7
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2025-01-14 17:29:25 +0530

    Update Highspacebetter.py

--- Arrays2/9.FInd duplicate no/Highspacebetter.py
+++ Arrays2/9.FInd duplicate no/Highspacebetter.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        frequency = {}
+        for i in nums:
+            if i in frequency:
+                frequency[i] = frequency[i] + 1
+                return i
+            else:
+                frequency[i] = 1
