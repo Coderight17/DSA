50aa8f0f2097b07feb2584712fd3a23ab8415060
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2025-02-15 01:57:20 +0530

    Update optimal.py

--- Arrays3/18ReversePairs/optimal.py
+++ Arrays3/18ReversePairs/optimal.py
@@ -8,3 +8,43 @@
 #   else:
 #       j = j+1
 #   return count -> Why does this not work?
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        def getcount(nums, low, mid, high):
+            i = low
+            j = mid+1
+            count = 0
+            for i in range(low, mid+1):
+                while j <= high and nums[i] > 2*nums[j]:
+                    j = j+1
+                count = count+j-mid-1
+            return count
+        def merge(nums, low, mid, high):
+            i = low
+            j = mid+1
+            t = []
+            while i <= mid and j <=high:
+                if nums[i] > nums[j]:
+                    t.append(nums[j])
+                    j = j+1
+                else:
+                    t.append(nums[i])
+                    i = i+1
+            while i <= mid:
+                t.append(nums[i])
+                i = i+1
+            while j <= high:
+                t.append(nums[j])
+                j = j+1
+            for i in range(low, high+1):
+                nums[i] = t[i-low]
+        def mergesort(nums, low, high):
+            count = 0
+            if low < high:
+                mid = (low+high)//2
+                count = count + mergesort(nums, low, mid)
+                count = count + mergesort(nums, mid+1, high)
+                count = count + getcount(nums, low, mid, high)
+                merge(nums, low, mid, high)
+            return count
+        return mergesort(nums, 0, len(nums)-1)
