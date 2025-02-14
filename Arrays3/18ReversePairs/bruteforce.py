5bd17550b201e2fc0f977a91bf9b5857c6d8dba8
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2025-02-14 21:41:10 +0530

    Update bruteforce.py

--- Arrays3/18ReversePairs/bruteforce.py
+++ Arrays3/18ReversePairs/bruteforce.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] > 2*nums[j]:
+                    count = count + 1
+        return count
+# Time limit exceeded(32/140 test cases passed)
