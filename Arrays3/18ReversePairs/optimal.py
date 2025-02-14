edabd6855aa46bff191a9d01ecf1e63611615c0a
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2025-02-15 01:37:35 +0530

    Create optimal.py

---
+++ Arrays3/18ReversePairs/optimal.py
@@ -0,0 +1,10 @@
+# i = low
+# j = mid+1
+# count = 0
+# while i <= mid and j <= high:
+#   if nums[i] > 2*nums[j]:
+#       count = count+mid-i+1
+#       i = i+1
+#   else:
+#       j = j+1
+#   return count -> Why does this not work?
