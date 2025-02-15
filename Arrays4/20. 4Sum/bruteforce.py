82ba66dc9f54f1719e77194e87d925f6c6f72c6c
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2025-02-15 17:49:55 +0530

    Update bruteforce.py

--- Arrays4/20. 4Sum/bruteforce.py
+++ Arrays4/20. 4Sum/bruteforce.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        st = set()
+        for i in range(len(nums)-3):
+            for j in range(i+1,len(nums)-2 ):
+                for k in range(j+1, len(nums)-1):
+                    for l in range(k+1, len(nums)):
+                        sum = nums[i] + nums[j]
+                        sum = sum + nums[k]
+                        sum = sum + nums[l]
+                        if sum == target:
+                            temp = [nums[i], nums[j], nums[k], nums[l]]
+                            temp.sort()
+                            st.add(tuple(temp))
+        final_array = [list(x) for x in st]      
+        return final_array
