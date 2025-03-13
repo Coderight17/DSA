17284264be54cb360e02b1d62f516eea46d92903
Coderight17 <161934530+Coderight17@users.noreply.github.com>
2025-03-13 22:04:52 +0530

    Create solution.py

---
+++ LinkedList1/30.Delete Node in a Linked List/solution.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+#41/41 test cases passed
+#Runtime 46ms beats 24.74%
+#Memory 17.89MB beats 93.24%
